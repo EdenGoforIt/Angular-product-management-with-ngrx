b6c4f615b556154d1fecac881b9f2be77c271e10
EdenPark <edengoforit@gmail.com>
2022-05-20 21:02:51 +1200

    annual chart component, css, html files created

---
+++ src/app/home/dashboard/annual-sale-chart/annual-sale-chart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eden-annual-sale-chart',
+  templateUrl: './annual-sale-chart.component.html',
+  styleUrls: ['./annual-sale-chart.component.scss'],
+})
+export class AnnualSalesChartComponent {}
