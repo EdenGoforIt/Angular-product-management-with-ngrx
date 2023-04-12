a9b1cb83f99239ee2713aa459bfa4de73f438b27
EdenPark <edengoforit@gmail.com>
2023-04-12 20:34:30 +1200

    changed anguar cli to NX

--- src/app/home/dashboard/annual-sale-chart/annual-sale-chart.component.ts
+++ src/app/home/dashboard/annual-sale-chart/annual-sale-chart.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import { DashboardService } from '../dashboard.service';
 @Component({
   selector: 'app-annual-sale-chart',
@@ -9,20 +8,20 @@ import { DashboardService } from '../dashboard.service';
 export class AnnualSalesChartComponent implements OnInit {
   errorMessage: string;
 
-  public salesChartData: ChartDataset[] = [{ data: [], label: 'Total Sales' }];
+  // public salesChartData: ChartDataset[] = [{ data: [], label: 'Total Sales' }];
 
 
-  public salesChartOptions: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-  };
+  // public salesChartOptions: ChartOptions = {
+  //   responsive: true,
+  //   maintainAspectRatio: false,
+  // };
 
-  public salesChartLegend = true;
-  public salesChartType: ChartType = 'line';
-  public salesChartPlugins = [];
+  // public salesChartLegend = true;
+  // public salesChartType: ChartType = 'line';
+  // public salesChartPlugins = [];
 
-  @ViewChild('annualSaleChart') private canvas: ElementRef;
-  constructor(private dashboardService: DashboardService) {}
+  // @ViewChild('annualSaleChart') private canvas: ElementRef;
+  // constructor(private dashboardService: DashboardService) {}
 
   ngOnInit() {
     // this.dashboardService.getSalesByMonth().subscribe({
